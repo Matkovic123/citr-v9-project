11f509100657adc60195371c72f17cbf3d6f2347
tmatkovic <toni.matkovic0@gmail.com>
2025-02-05 13:17:25 +0000

    Fixed TanStack devtools

--- mine/src/routes/__root.jsx
+++ mine/src/routes/__root.jsx
@@ -11,13 +11,16 @@ export const Route = createRootRoute({
   component: () => {
     const cartHook = useState([]);
     return (
-      <CartContext.Provider value={cartHook}>
-        <div>
-          <Header />
-          <Outlet />
-          <PizzaOfTheDay />
-        </div>
-      </CartContext.Provider>
+      <>
+        <CartContext.Provider value={cartHook}>
+          <div>
+            <Header />
+            <Outlet />
+            <PizzaOfTheDay />
+          </div>
+        </CartContext.Provider>
+        <TanStackRouterDevtools />
+      </>
     );
   },
 });
